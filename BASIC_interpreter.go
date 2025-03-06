ed38c681f4ae27a9b51b15179a63c207548edc52
jnazario17 <151779220+jnazario17@users.noreply.github.com>
2025-03-06 03:55:58 -0500

    Update BASIC_interpreter.go
    finished let, print, and println

--- BASIC_interpreter.go
+++ BASIC_interpreter.go
@@ -74,10 +74,130 @@ func interpret(index int) int {
 	return index
 }
 
+func isInteger(str string) bool {
+
+	_, ok := strconv.Atoi(str)
+	return ok == nil
+
+}
+
 // LET X = <ARITHMETIC_STATEMENT>
 // <ARITHMETIC_STATEMENT> is one of the following: X, X + Y, X - Y, X * Y, or X / Y
 func basicLet(line statement) {
 
+	var letter string
+	letter = string(line.args[0])
+	var strArray []string
+	strArray = strings.Fields(line.args)
+	var firstTerm string
+	var secondTerm string
+	var intFirstTerm int
+	var intSecondTerm int
+
+	// This if statement checks if there are any operations and if not, stores the value in variable
+	if len(strArray) < 4 {
+		if isInteger(strArray[2]) {
+			variables[letter], _ = strconv.Atoi(strArray[2])
+		} else {
+			variables[letter] = variables[strArray[2]]
+		}
+		return
+	}
+
+	firstTerm = strArray[2]
+	secondTerm = strArray[4]
+	intFirstTerm = 0
+	intSecondTerm = 0
+
+	// Processing Operations
+	if strings.Contains(line.args, "+") {
+		if isInteger(firstTerm) && isInteger(secondTerm) {
+			intFirstTerm, _ = strconv.Atoi(firstTerm)
+			intSecondTerm, _ = strconv.Atoi(secondTerm)
+			variables[letter] = intFirstTerm + intSecondTerm
+
+		} else if isInteger(firstTerm) {
+			intFirstTerm, _ = strconv.Atoi(firstTerm)
+			intSecondTerm = variables[secondTerm]
+			variables[letter] = intFirstTerm + intSecondTerm
+
+		} else if isInteger(secondTerm) {
+			intSecondTerm, _ = strconv.Atoi(secondTerm)
+			intFirstTerm = variables[firstTerm]
+			variables[letter] = intFirstTerm + intSecondTerm
+
+		} else {
+			intSecondTerm = variables[secondTerm]
+			intFirstTerm = variables[firstTerm]
+			variables[letter] = intFirstTerm + intSecondTerm
+		}
+
+	} else if strings.Contains(line.args, "-") {
+		if isInteger(firstTerm) && isInteger(secondTerm) {
+			intFirstTerm, _ = strconv.Atoi(firstTerm)
+			intSecondTerm, _ = strconv.Atoi(secondTerm)
+			variables[letter] = intFirstTerm - intSecondTerm
+
+		} else if isInteger(firstTerm) {
+			intFirstTerm, _ = strconv.Atoi(firstTerm)
+			intSecondTerm = variables[secondTerm]
+			variables[letter] = intFirstTerm - intSecondTerm
+
+		} else if isInteger(secondTerm) {
+			intSecondTerm, _ = strconv.Atoi(secondTerm)
+			intFirstTerm = variables[firstTerm]
+			variables[letter] = intFirstTerm - intSecondTerm
+
+		} else {
+			intSecondTerm = variables[secondTerm]
+			intFirstTerm = variables[firstTerm]
+			variables[letter] = intFirstTerm - intSecondTerm
+		}
+
+	} else if strings.Contains(line.args, "*") {
+		if isInteger(firstTerm) && isInteger(secondTerm) {
+			intFirstTerm, _ = strconv.Atoi(firstTerm)
+			intSecondTerm, _ = strconv.Atoi(secondTerm)
+			variables[letter] = intFirstTerm * intSecondTerm
+
+		} else if isInteger(firstTerm) {
+			intFirstTerm, _ = strconv.Atoi(firstTerm)
+			intSecondTerm = variables[secondTerm]
+			variables[letter] = intFirstTerm * intSecondTerm
+
+		} else if isInteger(secondTerm) {
+			intSecondTerm, _ = strconv.Atoi(secondTerm)
+			intFirstTerm = variables[firstTerm]
+			variables[letter] = intFirstTerm * intSecondTerm
+
+		} else {
+			intSecondTerm = variables[secondTerm]
+			intFirstTerm = variables[firstTerm]
+			variables[letter] = intFirstTerm * intSecondTerm
+		}
+
+	} else if strings.Contains(line.args, "/") {
+		if isInteger(firstTerm) && isInteger(secondTerm) {
+			intFirstTerm, _ = strconv.Atoi(firstTerm)
+			intSecondTerm, _ = strconv.Atoi(secondTerm)
+			variables[letter] = intFirstTerm / intSecondTerm
+
+		} else if isInteger(firstTerm) {
+			intFirstTerm, _ = strconv.Atoi(firstTerm)
+			intSecondTerm = variables[secondTerm]
+			variables[letter] = intFirstTerm / intSecondTerm
+
+		} else if isInteger(secondTerm) {
+			intSecondTerm, _ = strconv.Atoi(secondTerm)
+			intFirstTerm = variables[firstTerm]
+			variables[letter] = intFirstTerm / intSecondTerm
+
+		} else {
+			intSecondTerm = variables[secondTerm]
+			intFirstTerm = variables[firstTerm]
+			variables[letter] = intFirstTerm / intSecondTerm
+		}
+	}
 }
 
 // IF <CONDITION> THEN GOTO L
@@ -90,13 +210,14 @@ func basicIf(line statement, index int) int {
 // <PRINT_STATEMENT> is either a variable name or a literal string delimited by double quotes
 // Inside the quotes, the string contains only alphanumeric characters (a-z, A-Z, 0-9) and spaces
 func basicPrint(line statement) {
-	// condition for printing a string
-	if len(line.args) > 0 && line.args[0] == '"' && line.args[len(line.args)-1] == '"' {
-		fmt.Print(line.args[1 : len(line.args)-1])
+
+	// condition for printing a string; the reason it's compared to 34 is because str[index] returns the byte value
+	if len(line.args) > 0 && line.args[0] == 34 && line.args[len(line.args)-1] == 34 {
+		print(line.args[1 : len(line.args)-1])
 	} else if val, ok := variables[line.args]; ok { // condition for printing a variable that exists
-		fmt.Print(val)
-	} else {
-		// handle misinput here
+		print(val)
+	} else if val, ok := undefinedVariables[line.args]; ok {
+		print(val)
 	}
 }
 
@@ -104,14 +225,15 @@ func basicPrint(line statement) {
 // <PRINT_STATEMENT> is either a variable name or a literal string delimited by double quotes
 // Inside the quotes, the string contains only alphanumeric characters (a-z, A-Z, 0-9) and spaces
 func basicPrintln(line statement) {
-	// condition for printing a string
-	if len(line.args) > 0 && line.args[0] == '"' && line.args[len(line.args)-1] == '"' {
-		fmt.Println(line.args[1 : len(line.args)-1])
+
+	// condition for printing a string; the reason it's compared to 34 is because str[index] returns the byte value
+	if len(line.args) > 0 && line.args[0] == 34 && line.args[len(line.args)-1] == 34 {
+		println(line.args[1 : len(line.args)-1])
 	} else if val, ok := variables[line.args]; ok { // condition for printing a variable that exists
-		fmt.Println(val)
-	} else {
-		// handle misinput here
-	}
+		println(val)
+	} else if val, ok := undefinedVariables[line.args]; ok {
+		println(val)
+	} else
 }
 
 func main() {
